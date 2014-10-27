56e5f7d0191eb17bce28b2bec31d2ee1e98c3ab2
Matt Schley <msschley@gmail.com>
2014-10-27 11:45:57 -0500

    updated metrics

--- metrics.js
+++ metrics.js
@@ -22,13 +22,10 @@ for(i=0; i<userList.length; i++){
 	var last_tweet_time_date = userList[i].status.created_at;
 
 	// add metric data to metrics attribute in JSON object
-<<<<<<< HEAD
+
 	listMemberData.users[i].metrics = {"followers": followers_count, "following": following, "ff_ratio": ff_ratio, "verified" : verified, "last_active" : last_tweet_time_date, "num_lists_on" : listed_count, "how_often_tweet" : "tbd", 
 	"created_at": created_at, "location": location, "statuses_count": statuses_count};
 
-=======
-	listMemberData.users[i].metrics = {"ff_ratio": ff_ratio, "verified" : verified, "last_active" : last_tweet_time_date, "num_lists_on" : listed_count, "how_often_tweet" : "tbd"};
->>>>>>> 7d56e941589b8f0f68e2eff3380c80222a5e93f2
 }
 return listMemberData;
 console.log(userList);
