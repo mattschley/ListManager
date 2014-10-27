f24ea800a2cb4b57333b71259cce2e032c053c0d
Matt Schley <msschley@gmail.com>
2014-10-26 20:12:28 -0500

    metrics.js generates all basic metrics for all users in a list

--- metrics.js
+++ metrics.js
@@ -3,44 +3,29 @@
   MAKE FUNCTIONS UP HERE
 
 */
-function logUsers(){
-
-for(i=0; i<listMemberData.users.length; i++){
-	console.log(listMemberData.users[i]);	
-}
-
-}
 
 module.exports = function(listMemberData){
  
 var userList = listMemberData.users; 
 
-var user = userList[0];
-var location = userList[0].location;
-var followers_count = userList[0].followers_count;
-var following = userList[0].friends_count;
-var ff_ratio = followers_count/following;
-var created_at = userList[0].created_at;
-var verified = userList[0].verified; 
-var listed_count = userList[0].listed_count; 
-var statuses_count = userList[0].statuses_count; 
-var status = userList[0].status; 
-var last_tweet_time_date = userList[0].status.created_at;
+for(i=0; i<userList.length; i++){
+	var user = userList[i];
+	var location = userList[i].location;
+	var followers_count = userList[i].followers_count;
+	var following = userList[i].friends_count;
+	var ff_ratio = followers_count/following;
+	var created_at = userList[i].created_at;
+	var verified = userList[i].verified; 
+	var listed_count = userList[i].listed_count; 
+	var statuses_count = userList[i].statuses_count; 
+	var status = userList[i].status; 
+	var last_tweet_time_date = userList[i].status.created_at;
 
-listMemberData.users[0].metrics = {"ff_ratio" : ff_ratio};
-listMemberData.users[0].metrics = {"verified" : verified};
-listMemberData.users[0].metrics = {"last_active" : last_tweet_time_date};
-listMemberData.users[0].metrics = {"num_lists_on" : listed_count };
-listMemberData.users[0].metrics = {"how_often_tweet" : "tbd"};
+	// add metric data to metrics attribute in JSON object
+	listMemberData.users[i].metrics = {"ff_ratio": ff_ratio, "verified" : verified, "last_active" : last_tweet_time_date, "num_lists_on" : listed_count, "how_often_tweet" : "tbd"};
 
-// console.log(location);
-// console.log(ff_ratio);
-// console.log(created_at);
-// console.log(verified);
-// console.log(listed_count);
-// console.log(statuses_count); 
-// console.log(last_tweet_time_date);
-console.log(user);
+}
+console.log(userList);
   /*
     CALL FUNCTIONS HERE
   */
