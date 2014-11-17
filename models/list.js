a4f700c33371d9fb369a796fdc9338e5d6de6cef
davidmichaelryan <davidryan2015@u.northwestern.edu>
2014-11-17 01:58:46 -0600

    working on database

--- models/list.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var ListSchema = new Schema({
-  username: String,
-  listname: String
-});
-
-module.exports = mongoose.model('List', ListSchema);
--- routes.js
+++ routes.js
@@ -1,7 +1,6 @@
 var Twit = require ('twit');
 var Metrics = require('./metrics');
 var Statuses = require('./statuses');
-var listDB = require('./models/list.js');
 var tweetDB = require('./models/tweet.js');
 var mongoose = require('mongoose');
 
@@ -27,93 +26,138 @@ module.exports = function(app) {
     next();
   });
 
+  // app.get('/'){
+    
+  //   landing page
+    
+  // }
+
   app.get('/', function(req, res){
-//    return;
-    //list id for cg3ntry/northwestern: 172176744
-
-    var userName = 'funnelist338';
-    var listName = 'Golf';
-
+    var userName = 'ninatypewriter';
+    var listName = 'Microsoft';
+    /*
+      ^^^^ going to get refactored ^^^^
+    */
     var listData;
     var userData;
     var statusData;
 
-    T.get('lists/members', {slug: listName, owner_screen_name: userName, count: 5000}, function(err, data, response){
-    
+    var d = new Date();
+    var daysAgo = d.setDate(d.getDate() - 2);
+
+    /*
+      GET TO 500 TWEETS?
+  
+    */
+
+
+    var addToDB = function(listname, username, max_ID){
+      T.get('lists/statuses', {slug: listname, owner_screen_name: username, include_rts: false, max_id: max_ID, count: 300}, function(err, statusData, response){
+        if(err){
+          console.log("ERR IS HERE: "+err);
+        }
+
+        var oldestTweetId = max_ID;
+        var oldestTweetedAt = null;
+        var oldestTweet = null;
+
+        console.log(statusData.length);
+
+        for(var i = 0; i < statusData.length; i++){
+          var tweet = new tweetDB();
+          tweet.listusername = username;
+          tweet.listname = listname;
+          tweet.username = statusData[i].user.screen_name;
+          tweet.tweetid = statusData[i].id;
+          tweet.tweet = statusData[i].text;
+          tweet.tweeted_at = statusData[i].created_at;
+          tweet.urls = statusData[i].entities.urls;
+          tweet.mentions = statusData[i].entities.user_mentions;
+          tweet.favorites = statusData[i].favorite_count;
+          tweet.retweets = statusData[i].retweet_count;
+          // tweet.save(function(err){
+          // if(err)
+          //     console.log ("TWEET POSTING ERROR");
+          // });
+          if (oldestTweetId > statusData[i].id){
+            oldestTweetId = statusData[i].id;
+            oldestTweetedAt = statusData[i].created_at;
+            oldestTweet = statusData[i];
+
+          }
+        }
+        console.log({oldestId: oldestTweetId, oldestDate: oldestTweetedAt});
+        return {oldestId: oldestTweetId, oldestDate: oldestTweetedAt};
+      });
+    }
+
+
+    tweetDB.find({listusername: userName, listname: listName}).where('tweeted_at').gt(daysAgo).exec(function(err, data){
       if(err){
-        console.log("ERR IS: "+err);
+        console.log("there was an error");
+      }
+      if (data.length == 0){
+        console.log('there was no data');
+
+        var x = addToDB(listName, userName, 532989025553248256);
+
+      }
+      else{
+        console.log('there was data');
+        console.log(data);
       }
 
-      listData = data;  
-
-      T.get('users/show', {screen_name: userName}, function(err, data, response){
-        userData = data;
-        listData.user_info = userData;
-        var dataWithMetrics = new Metrics(listData);
-        tweetDB.find().lean().exec(function(err, data) {
-          if(err)
-            console.log('err accessing tweetdb');
-          
-          var timelineData = data;
-          
-          console.log(JSON.stringify(dataWithMetrics.users[0]));
-
-          res.locals = {
-            timeline: timelineData,
-            users: dataWithMetrics.users
-          };
-          res.render('testindex', {
-            title: '@' + userName + '/' + listName,
-            partials: {}
-          });
-        });
-      })
     });
+
+    // T.get('lists/members', {slug: listName, owner_screen_name: userName, count: 5000}, function(err, data, response){
+    
+    //   if(err){
+    //     console.log("ERR IS: "+err);
+    //   }
+
+    //   listData = data;  
+
+    //   T.get('users/show', {screen_name: userName}, function(err, data, response){
+    //     userData = data;
+    //     listData.user_info = userData;
+    //     var dataWithMetrics = new Metrics(listData);
+        
+    //     tweetDB.find().lean().exec( function(err, data) {
+    //       if(err)
+    //         console.log('err accessing tweetdb');
+          
+    //       var timelineData = data;
+          
+    //       res.locals = {
+    //         timeline: timelineData,
+    //         users: dataWithMetrics.users
+    //       };
+    //       res.render('index', {
+    //         title: '@' + userName + '/' + listName,
+    //         partials: {}
+    //       });
+    //     });
+    //   })
+    // });
   });
 
-  app.get('/tweetdb', function(req, res){
-    tweetDB.find().lean().exec(function(err, data) {
-      if(err)
-        console.log('err accessing tweetdb');
-      
-      //console.log(data);
-      //var prettyData = JSON.stringify(data, null, "\t");
-      var prettyData = JSON.stringify(data);
-
-      res.locals = {tweetData: prettyData};
-      res.render('tweetdb', {
-        title: 'Home',
-        partials: {}
-      });
-    });
-  });
 
   app.get('/update', function(req, res){
 
     var userName = 'funnelist338';
     var listName = 'Golf';
 
-    //last update 2:50am, https://twitter.com/NBA/status/531687734604492800
-    var sinceid = 531687734604492800; //
-     
-
-
-    T.get('lists/statuses', {slug: listName, owner_screen_name: userName, since_id: sinceid, include_rts: false}, function(err, data, response){
+    T.get('lists/statuses', {slug: listName, owner_screen_name: userName, include_rts: true, count: 100}, function(err, data, response){
       if(err){
         console.log("ERR IS HERE: "+err);
       }
       statusData = data;  
-//      var dataWithStatuses = new Statuses(statusData);
 
-      console.log(JSON.stringify(statusData));
-
-      var list = new listDB();
-      list.username = userName;
-      list.listname = listName;
-      list.save(function(err){
-        if(err)
-          console.log ("LIST POSTING ERROR");
-      });
+      //console.log(JSON.stringify(statusData));   
+      
+      console.log(statusData.length);
+      console.log(statusData[0].user.screen_name);
+      console.log(statusData[statusData.length - 1]);
 
       for(var i = 0; i < statusData.length; i++){
         var tweet = new tweetDB();
@@ -127,11 +171,15 @@ module.exports = function(app) {
         tweet.mentions = statusData[i].entities.user_mentions;
         tweet.favorites = statusData[i].favorite_count;
         tweet.retweets = statusData[i].retweet_count;
+
+        console.log(tweet+"\n\n\n");
+        
         tweet.save(function(err){
         if(err)
             console.log ("TWEET POSTING ERROR");
         });
       }
+
       console.log("FINISHED ADDING ALL THE TWEETS");    
       res.locals = {user_data: "updated tweets"}
       res.render('list', {
